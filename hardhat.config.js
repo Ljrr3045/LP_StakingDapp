9064312b184ab061098cfd6a124df108db62f23e
Luis Rivero <ljrr3045@gmail.com>
2022-04-13 14:03:31 -0430

    First UpDate

---
+++ hardhat.config.js
@@ -0,0 +1,59 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require('@openzeppelin/hardhat-upgrades');
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require('hardhat-deploy');
+require("dotenv").config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  solidity: {
+    compilers: [{
+      version: "0.5.16"
+    },
+    {
+      version: "0.7.6"
+    },
+    {
+      version: "0.8.8"
+    }]
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.MAINNET_URL, 
+        blockNumber: 14578717,
+      }
+    },
+    // rinkeby: {
+    //   url: process.env.RINKEBY_URL,
+    //   accounts:{
+    //     mnemonic: process.env.MNEMONIC
+    //   },
+    // },
+  },
+  etherscan: {
+    apiKey: {
+      rinkeby: process.env.ETHERSCAN_API_KEY
+    }
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: 0,
+    user1: 1,
+    user2: 2,
+  },
+};
