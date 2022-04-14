37f5771780497f6262046eebcb296e6dd3d01c32
Luis Rivero <ljrr3045@gmail.com>
2022-04-13 20:13:53 -0430

    UpDate V1.1

--- contracts/StakeContract.sol
+++ contracts/StakeContract.sol
@@ -1,19 +1,68 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >=0.5.16<0.9.0;
+pragma solidity >=0.8.0<0.9.0;
 
-import "synthetix/contracts/StakingRewards.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract StakeContract is StakingRewards{
+contract StakeContract {
     
-    constructor(
-        address _owner,
-        address _rewardsDistribution,
-        address _rewardsToken,
-        address _stakingToken
-    ) public StakingRewards(
-        _owner,
-        _rewardsDistribution,
-        _rewardsToken,
-        _stakingToken
-    ){}
+    IERC20 public rewardsToken;
+    IERC20 public stakingToken;
+
+    uint public rewardRate = 100;
+    uint public lastUpdateTime;
+    uint public rewardPerTokenStored;
+
+    mapping(address => uint) public userRewardPerTokenPaid;
+    mapping(address => uint) public rewards;
+
+    uint private _totalSupply;
+    mapping(address => uint) private _balances;
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = block.timestamp;
+
+        rewards[account] = earned(account);
+        userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        _;
+    }
+
+    constructor(address _stakingToken, address _rewardsToken) {
+        stakingToken = IERC20(_stakingToken);
+        rewardsToken = IERC20(_rewardsToken);
+    }
+
+    function rewardPerToken() internal view returns (uint) {
+        if (_totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored +
+            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) / _totalSupply);
+    }
+
+    function earned(address account) internal view returns (uint) {
+        return
+            ((_balances[account] *
+                (rewardPerToken() - userRewardPerTokenPaid[account])) / 1e18) +
+            rewards[account];
+    }
+
+    function stake(uint _amount) external updateReward(msg.sender) {
+        _totalSupply += _amount;
+        _balances[msg.sender] += _amount;
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function withdraw(uint _amount) external updateReward(msg.sender) {
+        _totalSupply -= _amount;
+        _balances[msg.sender] -= _amount;
+        stakingToken.transfer(msg.sender, _amount);
+    }
+
+    function getReward() external updateReward(msg.sender) {
+        uint reward = rewards[msg.sender];
+        rewards[msg.sender] = 0;
+        rewardsToken.transfer(msg.sender, reward);
+    }
 }
--- test/stakeContract.test.js
+++ test/stakeContract.test.js
@@ -1,19 +1,75 @@
-const { expect } = require("chai");
+const { inputToConfig } = require("@ethereum-waffle/compiler");
+const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
+const daiAbi = require("./ContractJson/Dai.json");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("StakeContract", async ()=> {
+  let StakeContract, stakeContract, ErcToken, ercToken, dai, owner, per1, perDai;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  before(async ()=> {
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    await hre.network.provider.request({ method: "hardhat_impersonateAccount",params: ["0x820c79d0b0c90400cdd24d8916f5bd4d6fba4cc3"],});
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    dai = await new ethers.Contract( "0x6B175474E89094C44Da98b954EedeAC495271d0F" , daiAbi);
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    ErcToken = await ethers.getContractFactory("ErcToken");
+    ercToken = await ErcToken.deploy("HouseToken", "HT");
+
+    StakeContract = await ethers.getContractFactory("StakeContract");
+    stakeContract = await StakeContract.deploy(dai.address, ercToken.address);
+
+    [owner, per1] = await ethers.getSigners();
+    perDai = await ethers.getSigner("0x820c79d0b0c90400cdd24d8916f5bd4d6fba4cc3");
+
+    await network.provider.send("hardhat_setBalance", [
+      perDai.address,
+      ethers.utils.formatBytes32String("5000000000000000000"),
+    ]);
+
+    await dai.connect(perDai).transfer(per1.address, ethers.utils.parseEther("1000"));
+
+    expect(await dai.connect(per1).balanceOf(per1.address)).to.equal(ethers.utils.parseEther("1000"));
+  });
+
+  it("Stake Contract should receive reward token", async ()=> {
+
+    expect(await ercToken.connect(owner).balanceOf(stakeContract.address)).to.equal(0);
+
+    await ercToken.connect(owner).mint(stakeContract.address, ethers.utils.parseEther("100000"));
+
+    expect(await ercToken.connect(owner).balanceOf(stakeContract.address)).to.equal(ethers.utils.parseEther("100000"));
+  });
+
+  it("It should be possible to place money in the stake", async ()=> {
+
+    await dai.connect(per1).approve(stakeContract.address, ethers.utils.parseEther("1000"));
+
+    await stakeContract.connect(per1).stake(ethers.utils.parseEther("1000"));
+
+    expect(await dai.connect(per1).balanceOf(stakeContract.address)).to.equal(ethers.utils.parseEther("1000"));
+  });
+
+  it("It should be possible to withdraw the money in the stake", async ()=> {
+
+    await network.provider.send("evm_increaseTime", [604800]);
+    await network.provider.send("evm_mine");
+
+    await stakeContract.connect(per1).withdraw(ethers.utils.parseEther("1000"));
+
+    expect(await dai.connect(per1).balanceOf(stakeContract.address)).to.equal(0);
+    expect(await dai.connect(per1).balanceOf(per1.address)).to.equal(ethers.utils.parseEther("1000"));
+  });
+
+  it("It should be possible to withdraw the reward generated in the stake", async ()=> {
+
+    expect(await ercToken.connect(owner).balanceOf(per1.address)).to.equal(0);
+
+    await stakeContract.connect(per1).getReward();
+
+    let rewardUser = await ercToken.connect(owner).balanceOf(per1.address);
+    let balanceStaker = await ercToken.connect(owner).balanceOf(stakeContract.address);
+
+    expect(rewardUser).to.be.above(0);
+    expect(balanceStaker).to.be.below(ethers.utils.parseEther("100000"));
   });
 });
